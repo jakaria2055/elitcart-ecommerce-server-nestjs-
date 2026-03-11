fc5348edfbfd19bb2eb4790cdfacfe0fcacd3359
Jakaria Ahmed <jakaria88520@gmail.com>
2026-03-11 11:43:30 +0600

    get category by id, update category, delete category, category by slug

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import {
   ApiBearerAuth,
   ApiBody,
@@ -14,6 +26,7 @@ import { Role } from '@prisma/client';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { CategoryResponseDto } from './dto/category-response.dto';
 import { QueryCategoryDto } from './dto/query-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @ApiTags('Categories')
 @Controller('categories')
@@ -31,7 +44,7 @@ export class CategoryController {
   @ApiResponse({ status: 401, description: 'Unauthorized' })
   @ApiResponse({ status: 403, description: 'Forbidden' })
   async create(
-    createCategoryDto: CreateCategoryDto,
+    @Body() createCategoryDto: CreateCategoryDto,
   ): Promise<CategoryResponseDto> {
     return await this.categoryService.create(createCategoryDto);
   }
@@ -63,5 +76,83 @@ export class CategoryController {
   })
   async findAll(@Query() queryDto: QueryCategoryDto) {
     return await this.categoryService.findAll(queryDto);
+  }
+
+  //GET CATEGORY BY ID
+  @Get(':id')
+  @ApiOperation({ summary: 'Get Category By ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Category Details',
+    type: CategoryResponseDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Category not found',
+  })
+  async findOne(@Param('id') id: string): Promise<CategoryResponseDto> {
+    return await this.categoryService.findOne(id);
+  }
+
+  //GET CATEGORY BU SLUG
+  @Get('slug/:slug')
+  @ApiOperation({
+    summary: 'Get Category By Slug',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Category Details',
+    type: CategoryResponseDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Category Not Found',
+  })
+  async findBySlug(@Param('slug') slug: string): Promise<CategoryResponseDto> {
+    return await this.categoryService.findBySlug(slug);
+  }
+
+  //UPDATE CATEGORY (ADMIN ONLY)
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @ApiBearerAuth('JWT-auth')
+  @ApiOperation({ summary: 'Update category (Admin Only)' })
+  @ApiBody({
+    type: UpdateCategoryDto,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'category updated successfully',
+    type: CategoryResponseDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Category Not Found',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'Category slug already',
+  })
+  async update(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ): Promise<CategoryResponseDto> {
+    return await this.categoryService.update(id, updateCategoryDto);
+  }
+
+  //DELETE CATEGORY(ADMIN ONLY)
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @ApiBearerAuth('JWT-auth')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Delete Category By ID' })
+  @ApiResponse({
+    status: 400,
+    description: 'Cannot delete category with products',
+  })
+  async remove(@Param('id') id: string): Promise<{ message: string }> {
+    return await this.categoryService.remove(id);
   }
 }
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { CategoryResponseDto } from './dto/category-response.dto';
 import { Category, Prisma } from '@prisma/client';
 import { QueryCategoryDto } from './dto/query-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoryService {
@@ -90,6 +96,112 @@ export class CategoryService {
         totalPages: Math.ceil(total / limit),
       },
     };
+  }
+
+  //GET CATEGORY BY ID
+  async findOne(id: string): Promise<CategoryResponseDto> {
+    const category = await this.prisma.category.findUnique({
+      where: { id },
+      include: {
+        _count: {
+          select: { products: true },
+        },
+      },
+    });
+
+    if (!category) {
+      throw new NotFoundException('Category not found!');
+    }
+
+    return this.formateCategory(category, Number(category._count.products));
+  }
+
+  //GET CATEGORY BY SLUG
+  async findBySlug(slug: string): Promise<CategoryResponseDto> {
+    const category = await this.prisma.category.findUnique({
+      where: { slug },
+      include: {
+        _count: {
+          select: { products: true },
+        },
+      },
+    });
+
+    if (!category) {
+      throw new NotFoundException('Category Not Found');
+    }
+
+    return this.formateCategory(category, Number(category._count.products));
+  }
+
+  //UPDATE CATEGORY(ADMIN ONLY)
+  async update(
+    id: string,
+    updateCategoryDto: UpdateCategoryDto,
+  ): Promise<CategoryResponseDto> {
+    const existingCategory = await this.prisma.category.findUnique({
+      where: { id },
+    });
+
+    if (!existingCategory) {
+      throw new NotFoundException('Category not found');
+    }
+
+    if (
+      updateCategoryDto.slug &&
+      updateCategoryDto.slug !== existingCategory.slug
+    ) {
+      const slugToken = await this.prisma.category.findUnique({
+        where: { slug: updateCategoryDto.slug },
+      });
+
+      if (slugToken) {
+        throw new ConflictException(
+          `Category with slug ${updateCategoryDto.slug} already exists`,
+        );
+      }
+    }
+
+    const updateCategory = await this.prisma.category.update({
+      where: { id },
+      data: updateCategoryDto,
+      include: {
+        _count: {
+          select: { products: true },
+        },
+      },
+    });
+
+    return this.formateCategory(
+      updateCategory,
+      Number(updateCategory._count.products),
+    );
+  }
+
+  //DELETE CATEGORY (ADMIN ONLY)
+  async remove(id: string): Promise<{ message: string }> {
+    const category = await this.prisma.category.findUnique({
+      where: { id },
+      include: {
+        _count: {
+          select: { products: true },
+        },
+      },
+    });
+
+    if (!category) {
+      throw new NotFoundException('Category Not found');
+    }
+
+    if (category._count.products > 0) {
+      throw new BadRequestException(
+        `Can not delete category with ${category._count.products} products. Remove or reassign first`,
+      );
+    }
+
+    await this.prisma.category.delete({ where: { id } });
+
+    return { message: `Category Deleted successfully: ${id}` };
   }
 
   private formateCategory(
